645041bd485a73eb7f874bc191984783867dc9c9
Klimaszewska <klimaszewska.justyna@gmail.com>
2026-01-11 15:01:04 +0100

    fix tests for Invoice

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -1,30 +1,55 @@
 package pl.edu.agh.mwo.invoice;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Collection;
 
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
-    private Collection<Product> products;
+    private final Collection<Product> products = new ArrayList<>();
 
     public void addProduct(Product product) {
-        // TODO: implement
+        if (product == null) {
+            throw new IllegalArgumentException("value cannot be null");
+        }
+        this.products.add(product);
     }
 
     public void addProduct(Product product, Integer quantity) {
-        // TODO: implement
+        if (quantity <= 0) {
+            throw new IllegalArgumentException("Quantity must be greater than zero");
+        }
+        while (quantity > 0) {
+            this.products.add(product);
+            quantity--;
+        }
     }
 
     public BigDecimal getSubtotal() {
-        return null;
+        BigDecimal subtotal = BigDecimal.ZERO;
+        for (Product product : this.products) {
+            BigDecimal price = product.getPrice();
+            subtotal = subtotal.add(price);
+        }
+        return subtotal;
     }
 
     public BigDecimal getTax() {
-        return null;
+        BigDecimal totalTax = BigDecimal.ZERO;
+        for (Product product : this.products) {
+            BigDecimal tax = product.getPriceWithTax().subtract(product.getPrice());
+            totalTax = totalTax.add(tax);
+        }
+        return totalTax;
     }
 
     public BigDecimal getTotal() {
-        return null;
+        BigDecimal total = BigDecimal.ZERO;
+        for (Product product : this.products) {
+            BigDecimal price = product.getPriceWithTax();
+            total = total.add(price);
+        }
+        return total;
     }
 }
